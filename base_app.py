aa8a7b09c77be255fbe3e270e143e3c378c50f69
Oboho-Etuk <aniedibobohoetuk@uniuyo.edu.ng>
2022-12-08 18:49:15 +0100

    edited UI of base_app.py for streamlit. Tried adding SVM model but had a setback

--- base_app.py
+++ base_app.py
@@ -25,6 +25,10 @@
 import streamlit as st
 import joblib,os
 
+# Text Cleaning tools
+from textblob import TextBlob 
+import cleantext
+
 # Data dependencies
 import pandas as pd
 
@@ -33,7 +37,16 @@ news_vectorizer = open("resources/tfidfvect.pkl","rb")
 tweet_cv = joblib.load(news_vectorizer) # loading your vectorizer from the pkl file
 
 # Load your raw data
-raw = pd.read_csv("resources/train.csv")
+st.title("Data")
+st.subheader("Load Data to Classify")
+
+with st.expander('Analyze CSV'):
+	new_data = st.file_uploader('Upload a csv file')
+
+if new_data:
+	raw=pd.read_csv(new_data)
+else:
+	raw = pd.read_csv("resources/train.csv")
 
 # The main function where we will build the actual app
 def main():
@@ -44,12 +57,14 @@ def main():
 	st.title("Tweet Classifer")
 	st.subheader("Climate change tweet classification")
 
+
 	# Creating sidebar with selection box -
 	# you can create multiple pages this way
 	options = ["Prediction", "Information"]
 	selection = st.sidebar.selectbox("Choose Option", options)
 
 	# Building out the "Information" page
+
 	if selection == "Information":
 		st.info("General Information")
 		# You can read a markdown file from supporting resources folder
@@ -60,23 +75,52 @@ def main():
 			st.write(raw[['sentiment', 'message']]) # will write the df to the page
 
 	# Building out the predication page
+
 	if selection == "Prediction":
 		st.info("Prediction with ML Models")
+
 		# Creating a text box for user input
-		tweet_text = st.text_area("Enter Text","Type Here")
+		tweet_text = st.text_area("Enter Message","Type Here")
 
 		if st.button("Classify"):
 			# Transforming user input with vectorizer
 			vect_text = tweet_cv.transform([tweet_text]).toarray()
 			# Load your .pkl file with the model of your choice + make predictions
 			# Try loading in multiple models to give the user a choice
+			st.subheader("Logistic Regression Model")
 			predictor = joblib.load(open(os.path.join("resources/Logistic_regression.pkl"),"rb"))
 			prediction = predictor.predict(vect_text)
 
 			# When model has successfully run, will print prediction
 			# You can use a dictionary or similar structure to make this output
 			# more human interpretable.
-			st.success("Text Categorized as: {}".format(prediction))
+			if prediction==2:
+				st.success("Statement of fact")
+			if prediction==1:
+				st.success("Pro Climate Change Statement")
+			if prediction==0:
+				st.success("Neutral")
+			if prediction==-1:
+				st.success("Statement Unsure if Climate Change is Real")
+
+			st.subheader("Support Vector Machine Model")
+			predictor_svm = joblib.load(open(os.path.join("resources/tfidfvect.pkl"),"rb"))
+			prediction_svm = predictor_svm.predict(vect_text)
+			# When model has successfully run, will print prediction
+			# You can use a dictionary or similar structure to make this output
+			# more human interpretable.
+			if prediction_svm==2:
+				st.success("Statement of fact")
+			if prediction_svm==1:
+				st.success("Pro Climate Change Statement")
+			if prediction_svm==0:
+				st.success("Neutral")
+			if prediction_svm==-1:
+				st.success("Statement Unsure if Climate Change is Real")
+
+			# st.success("Text Categorized as: {}".format(prediction))
+	
+
 
 # Required to let Streamlit instantiate our web app.  
 if __name__ == '__main__':
